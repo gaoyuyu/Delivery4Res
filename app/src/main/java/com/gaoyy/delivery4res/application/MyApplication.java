e7063f4b8f599450b3c029b073a15282a144937e
gaoyuyu <740514999@qq.com>
2017-06-09 22:04:16 +0800

    Add AutoUpgrade SDK

--- app/src/main/java/com/gaoyy/delivery4res/application/MyApplication.java
+++ app/src/main/java/com/gaoyy/delivery4res/application/MyApplication.java
@@ -1,10 +1,13 @@
 package com.gaoyy.delivery4res.application;
 
 import android.app.Application;
+import android.os.Environment;
 
+import com.gaoyy.delivery4res.R;
 import com.gaoyy.delivery4res.api.Constant;
 import com.gaoyy.delivery4res.api.RetrofitService;
-import com.tencent.bugly.crashreport.CrashReport;
+import com.tencent.bugly.Bugly;
+import com.tencent.bugly.beta.Beta;
 
 
 /**
@@ -22,6 +25,8 @@ public class MyApplication extends Application
 
         RetrofitService.init(this);
 
+        upgradeSetting();
+
         /**
          * 第三个参数为SDK调试模式开关，调试模式的行为特性如下：
          输出详细的Bugly SDK的Log；
@@ -29,7 +34,66 @@ public class MyApplication extends Application
          自定义日志将会在Logcat中输出。
          建议在测试阶段建议设置成true，发布时设置为false。
          */
-        CrashReport.initCrashReport(getApplicationContext(), Constant.BUGLY_APP_ID, true);
+        Bugly.init(getApplicationContext(), Constant.BUGLY_APP_ID, true);
+    }
+
+    /**
+     * Beta高级设置
+     */
+    private void upgradeSetting()
+    {
+        /**
+         * true表示app启动自动初始化升级模块；
+         * false不好自动初始化
+         * 开发者如果担心sdk初始化影响app启动速度，可以设置为false
+         * 在后面某个时刻手动调用
+         */
+        Beta.autoInit = true;
+
+        /**
+         * true表示初始化时自动检查升级
+         * false表示不会自动检查升级，需要手动调用Beta.checkUpgrade()方法
+         */
+        Beta.autoCheckUpgrade = true;
+
+        /**
+         * 设置升级周期为60s（默认检查周期为0s），60s内SDK不重复向后天请求策略
+         */
+        Beta.initDelay = 1 * 1000;
+
+        /**
+         * 设置通知栏大图标，largeIconId为项目中的图片资源；
+         */
+        Beta.largeIconId = R.mipmap.ic_launcher;
+
+        /**
+         * 设置状态栏小图标，smallIconId为项目中的图片资源id;
+         */
+        Beta.smallIconId = R.mipmap.ic_launcher;
+
+
+        /**
+         * 设置更新弹窗默认展示的banner，defaultBannerId为项目中的图片资源Id;
+         * 当后台配置的banner拉取失败时显示此banner，默认不设置则展示“loading“;
+         */
+        Beta.defaultBannerId = R.mipmap.ic_launcher;
+
+        /**
+         * 设置sd卡的Download为更新资源保存目录;
+         * 后续更新资源会保存在此目录，需要在manifest中添加WRITE_EXTERNAL_STORAGE权限;
+         */
+        Beta.storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+
+        /**
+         * 点击过确认的弹窗在APP下次启动自动检查更新时会再次显示;
+         */
+        Beta.showInterruptedStrategy = false;
+
+        /**
+         * 只允许在MainActivity上显示更新弹窗，其他activity上不显示弹窗;
+         * 不设置会默认所有activity都可以显示弹窗;
+         */
+//        Beta.canShowUpgradeActs.add(MainActivity.class);
     }
 
 
